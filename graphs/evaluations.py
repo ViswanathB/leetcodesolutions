d5d02c5d0fca5f963b839f6c9aec7058d74e5242
vish <visu.boga@gmail.com>
2023-10-19 20:34:19 -0400

    union, set problem

---
+++ graphs/evaluations.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/evaluate-division/description/
+
+Graph is a set of trees
+
+It is common to store union and set in the form of set and keep
+adding to sets and unifying sets for node in the same tree.
+
+There might be cases where ratios between the members of a one tree
+should be kept track of. This is one sucn example
+"""
+
+"""
+Adding the solution here as it is little bit tough to test the code:
+    
+https://leetcode.com/problems/evaluate-division/submissions/1079571950/
+"""
